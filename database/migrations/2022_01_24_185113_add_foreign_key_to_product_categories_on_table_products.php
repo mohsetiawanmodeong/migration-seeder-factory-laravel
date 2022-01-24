787d4a4bd1134d408434c825abd876260d18da5d
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-01-25 04:09:55 +0800

    first commit

---
+++ database/migrations/2022_01_24_185113_add_foreign_key_to_product_categories_on_table_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToProductCategoriesOnTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_product_category_id_foreign');
+        });
+    }
+}
